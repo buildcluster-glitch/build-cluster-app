0ae041597be9533c91a0db5921685e5156a841f0
buildcluster-glitch <buildcluster-glitch@users.noreply.github.com>
2026-05-31 23:14:34 +0900

    auto: 2026-05-31_23:14:34

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // オフラインでも localStorage データで動く + 起動が早い
 // 更新時はバージョンを上げる → ユーザー次回起動時にキャッシュ更新
 
-const CACHE = 'build-cluster-app-v95';
+const CACHE = 'build-cluster-app-v96';
 const ASSETS = [
   './',
   './index.html',
